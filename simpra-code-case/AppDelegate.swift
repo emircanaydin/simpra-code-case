15557647dea003bb6a6086d6550fd61cc27b5ab1
Emircan Aydın <emircanaydin973@gmail.com>
2023-08-08 14:59:10 +0300

    project setup

---
+++ simpra-code-case/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  simpra-code-case
+//
+//  Created by Emircan Aydın on 8.08.2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
